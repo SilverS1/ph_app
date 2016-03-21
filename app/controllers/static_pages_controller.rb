fa77a6cfe2b09157a41a1da6a2f4f3e5bfda70ff
Sarah Le Cornec <sarahlecornec123@gmail.com>
2016-03-21 12:04:37 +0000

    Minor styles made, comments hover fixed

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,7 +1,7 @@
 class StaticPagesController < ApplicationController
 
 	def home
-		@products = Product.all
+		@products = Product.all.order("created_at DESC").paginate(:per_page => 12, :page => params[:page])
 	end
 
 end
